ab151b22fee53bc6f8bf3ebbe4d421f6dee29f54
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-11-28 14:20:50 -0500

    Initial Commit

---
+++ SnowCalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCalls
+//
+//  Created by Vallamkonda, Sunaina on 2019-11-28.
+//  Copyright © 2019 Vallamkonda, Sunaina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
